5a91b2fcd3e2c927691b6a9e56089f787970d170
Greg Khmara <gkhmara@gmail.com>
2022-10-28 14:26:15 -0700

    create LandingPage Component
    co-authored-by: Anthony DiFalco <aodifalco@gmail.com>
    co-authored-by: Winnie Wang <weijwang18@gmail.com>
    co-authored-by: Todd Pangilinan <pang.todd@gmail.com>

---
+++ client/src/Components/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LandingPage() {
+
+  return (
+    <>
+      <h1>HaiCue World</h1>
+      <p>The place for Haikus</p>
+    </>
+  );
+
+};
+
+export default LandingPage;
