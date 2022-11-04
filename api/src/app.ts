2758282fe2e341cc9cd4aaff9ef85c4e38488cfa
Greg Khmara <gkhmara@gmail.com>
2022-11-04 16:55:54 -0700

    setup api endpoints for teams

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,39 +1,20 @@
 import { PrismaClient } from '@prisma/client';
+import teamsRouter from "./Routes/teams";
 import cors from 'cors';
 import express from 'express';
 
 const prisma = new PrismaClient();
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 
 app.use(express.json());
+
+app.use(teamsRouter);
 
 app.get('/users', async (req, res) => {
   const users = await prisma.users.findMany();
   res.json(users);
 });
 
-app.get('/teams', async (req, res) => {
-  const teams = await prisma.teams.findMany();
-  res.json(teams);
-});
-
-app.post('/teams', async (req, res) => {
-  const { teamName } = req.body;
-  const newTeam = async (teamName: string, teamLeaderId: number) => {
-    return await prisma.teams.create({
-      data: {
-        teamName,
-        teamLeaderId,
-        teamScore: 0,
-        gameId: 1
-      }
-    });
-  }
-  newTeam(teamName, 1);
-});
-
 export default app;
-
-
