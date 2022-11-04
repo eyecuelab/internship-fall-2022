1fb0ae40e487f430e73bbb0ed8a028c0e75c59aa
Greg Khmara <gkhmara@gmail.com>
2022-11-04 12:04:00 -0700

    testing out api calls

--- api/src/app.ts
+++ api/src/app.ts
@@ -12,6 +12,28 @@ app.use(express.json());
 app.get('/users', async (req, res) => {
   const users = await prisma.users.findMany();
   res.json(users);
-})
+});
+
+app.get('/teams', async (req, res) => {
+  const teams = await prisma.teams.findMany();
+  res.json(teams);
+});
+
+app.post('/teams', async (req, res) => {
+  const { teamName } = req.body;
+  const newTeam = async (teamName: string, teamLeaderId: number) => {
+    return await prisma.teams.create({
+      data: {
+        teamName,
+        teamLeaderId,
+        teamScore: 0,
+        gameId: 1
+      }
+    });
+  }
+  newTeam(teamName, 1);
+});
 
 export default app;
+
+
