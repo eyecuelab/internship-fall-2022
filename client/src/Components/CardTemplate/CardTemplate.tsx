ad1fd56be3f6a7995c183ee906e4676a939105dd
Todd Pangilinan <pang.todd@gmail.com>
2022-11-07 14:38:46 -0800

    WIP, switching driver.

--- client/src/Components/CardTemplate/CardTemplate.tsx
+++ client/src/Components/CardTemplate/CardTemplate.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Card from "@mui/material/Card";
+import React from 'react';
+import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
@@ -8,7 +8,16 @@ const CardTemplate = () => {
   return (
     <>
       <Card sx={{ display: 'flex' }}>
-        
+        <CardMedia 
+          component="img"
+          sx={{ width: 151 }}
+          image="oranges.jpg"
+          alt="an orange"
+        />
+        <CardContent>
+          <h1>I'm a computer.</h1>
+          <h1>Stop all the downloading.</h1>
+        </CardContent>
       </Card>
     </>
   )
@@ -16,3 +25,29 @@ const CardTemplate = () => {
 }
 
 export default CardTemplate;
+
+
+
+//   return (
+//     <Card sx={{ minWidth: 275 }}>
+//       <CardContent>
+//         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+//           Word of the Day
+//         </Typography>
+//         <Typography variant="h5" component="div">
+//           be{bull}nev{bull}o{bull}lent
+//         </Typography>
+//         <Typography sx={{ mb: 1.5 }} color="text.secondary">
+//           adjective
+//         </Typography>
+//         <Typography variant="body2">
+//           well meaning and kindly.
+//           <br />
+//           {'"a benevolent smile"'}
+//         </Typography>
+//       </CardContent>
+//       <CardActions>
+//         <Button size="small">Learn More</Button>
+//       </CardActions>
+//     </Card>
+//   );
