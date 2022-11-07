0b55498fc3ee1468ed82d5a441063c6c2083743d
Todd Pangilinan <pang.todd@gmail.com>
2022-11-07 13:52:18 -0800

    initial CardTemplate.tsx file
    Co-authored-by: Anthony DiFalco <aodifalco@gmail.com>
    Co-authored-by: Winnie Wang <weijwang18@gmail.com>
    Co-authored-by: Greg Khmara <gkhmara@gmail.com>

---
+++ client/src/Components/CardTemplate/CardTemplate.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
+const CardTemplate = () => {
+
+  return (
+    <>
+      <Card sx={{ display: 'flex' }}>
+        
+      </Card>
+    </>
+  )
+
+}
+
+export default CardTemplate;
