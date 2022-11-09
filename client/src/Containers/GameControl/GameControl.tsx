566ac055b0e18f5413194390be4831582efdff01
Anthony DiFalco <aodifalco@gmail.com>
2022-11-09 15:35:30 -0800

    create template gamecontrol parent component

---
+++ client/src/Containers/GameControl/GameControl.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../index.css';
+import { Card, CardContent, Button } from '@mui/material';
+import { Header, Content } from './styles';
+
+const ModControl = () => {
+
+  return (
+    <>
+      <Header><h4>Eyecue Haicue</h4></Header>
+    </>
+  );
+}
+
+export default ModControl
