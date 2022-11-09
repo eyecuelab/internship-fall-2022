718eb2e94a0f27c98d5c0991786acf451d77a11f
Anthony DiFalco <aodifalco@gmail.com>
2022-11-09 15:46:39 -0800

    import cardtemplate into gamecontainer

--- client/src/Containers/GameControl/GameControl.tsx
+++ client/src/Containers/GameControl/GameControl.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../index.css';
 import { Card, CardContent, Button } from '@mui/material';
 import { Header } from './styles';
+import CardTemplate from '../../Components/CardTemplate/CardTemplate';
 
 const ModControl = () => {
 
   return (
     <>
       <Header><h4>Eyecue Haicue</h4></Header>
+      <CardTemplate />
     </>
   );
 }
