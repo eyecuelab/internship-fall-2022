b7392d226a8850234bccd9f587175cd35f01b30c
Anthony DiFalco <aodifalco@gmail.com>
2022-11-10 09:50:15 -0800

    add brackets to teamoverlay values that will be filled conditionally

--- client/src/Components/TeamOverlay/TeamOverlay.tsx
+++ client/src/Components/TeamOverlay/TeamOverlay.tsx
@@ -4,13 +4,13 @@ const TeamOverlay = () => {
   return (
     <>
       <h3>Team</h3>
-      <h1>BLUEBERRY</h1>
+      <h1>{'BLUEBERRY'}</h1>
       <br />
       <h3>Points</h3>
-      <h1>3</h1>
+      <h1>{'3'}</h1>
       <br />
       <h3>Timer</h3>
-      <h1>47</h1>
+      <h1>{'47'}</h1>
     </>
   );
 }
