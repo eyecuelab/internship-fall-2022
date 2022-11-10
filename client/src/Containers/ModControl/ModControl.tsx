bc975c75c06d587302447234661c9e5e36391ced
Anthony DiFalco <aodifalco@gmail.com>
2022-11-09 16:03:04 -0800

    add template for conditionally rendering components in mod container

--- client/src/Containers/ModControl/ModControl.tsx
+++ client/src/Containers/ModControl/ModControl.tsx
@@ -1,36 +1,24 @@
-import React from 'react';
 import '../../index.css';
-import { Card, CardContent, Button } from '@mui/material';
-import { Header, Content } from './styles';
+import { Header } from './styles';
+import CardTemplate from '../../Components/CardTemplate/CardTemplate';
+import ModLogin from '../../Components/ModLogin/ModLogin';
 
 const ModControl = () => {
 
-  return (
-    <>
-      <Header><h4>Eyecue Haicue</h4></Header>
-      <Card sx={{ width: 1376, height: 820, minHeight: "fit-content", borderRadius: "15px", boxShadow: "0px 0px 75px #000", background: '#15586a', backgroundImage: 'url(./public/images/moderator_card_background.png)'}}>
-        <Content style={{ color: '#f9f9f9' }}>
-          <CardContent sx={{ display: 'flex', flexFlow: 'row wrap', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-            {/* example text, will be dynamically rendered */}
-            <div>
-            <h4>Moderator Login</h4>
-            <br />
-            <Button 
-              sx={{
-                height: '75px',
-                width: '100%',
-                color: '#363636',
-                border: '1px solid #363636',
-                borderRadius: '10px',
-            }}>
-              GOOGLE SIGN IN
-          </Button>
-          </div>
-          </CardContent>
-        </Content>
-      </Card>
-    </>
-  );
+  if (true) { 
+    return (
+      <>
+        <Header><h4>Eyecue Haicue</h4></Header>
+        <ModLogin />
+      </>
+    ); 
+  } else { 
+    return ( 
+      <>
+        <CardTemplate />
+      </>
+    ); 
+  }
 }
 
 export default ModControl
--- client/src/Containers/ModControl/styles.tsx
+++ client/src/Containers/ModControl/styles.tsx
@@ -5,15 +5,3 @@ export const Header = styled.div`
   display: block;
   text-align: right;
 `;
-
-export const Content = styled.div`
-  color: #363636;
-  height: 90%;
-  width: auto;
-  margin: auto;
-  display: block;
-  padding: 2.75%;
-  padding-left: 5.5%;
-  padding-right: 5.5%;
-  text-align: left;
-`;
