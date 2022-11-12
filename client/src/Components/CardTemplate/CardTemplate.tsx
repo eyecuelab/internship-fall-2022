9164c2e6e6540b4f3f3d0dfb1ce42967b6817c7e
Anthony DiFalco <aodifalco@gmail.com>
2022-11-11 20:31:19 -0800

    fix floating button issue in haikuform

--- client/src/Components/CardTemplate/CardTemplate.tsx
+++ client/src/Components/CardTemplate/CardTemplate.tsx
@@ -15,11 +15,21 @@ function CardTemplate() {
       <Header>
         <h4>Eyecue Haicue</h4>
       </Header>
-      <Card className="cardBanner">
+      <Card
+        sx={{
+          display: 'flex',
+          width: 1376,
+          minHeight: 820,
+          height: 'fit-content',
+          borderRadius: '15px',
+          boxShadow: '0px 0px 75px #000',
+          background: '#f6ede9',
+        }}
+      >
         <div style={{ position: 'relative' }}>
           <CardMedia
             component="div"
-            sx={{ width: '25rem', minHeight: '101%', height: '101%' }}
+            sx={{ width: 490, minHeight: '101%', height: '101%' }}
             style={{
               background: '#15586A',
               minHeight: '101%',
@@ -29,13 +39,13 @@ function CardTemplate() {
           <Overlay>
             {/* Components in the Overlay tag will likely be rendered with a switch statement */}
             {/* <TeamOverlay /> */}
-            {/* <ModOverlay /> */}
+            <ModOverlay />
           </Overlay>
         </div>
         <Content>
           <CardContent sx={{ height: '100%' }}>
             {/* Components in the CardContent tag will likely be rendered with switch statement */}
-            {/* <HaikuForm /> */}
+            <HaikuForm />
             {/* <ModGameList /> */}
           </CardContent>
         </Content>
