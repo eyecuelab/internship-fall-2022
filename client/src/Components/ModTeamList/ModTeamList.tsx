7863886e71beae134366e854d13b8bd49dd020bb
Greg Khmara <gkhmara@gmail.com>
2022-11-14 16:42:42 -0800

    add buttons to ModTeamList

--- client/src/Components/ModTeamList/ModTeamList.tsx
+++ client/src/Components/ModTeamList/ModTeamList.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { whiteButton, redButton } from '../componentStyles';
 
 function TeamList() {
+  whiteButton.width = '100%';
+  redButton.width = '100%';
   return (
     <>
       <Grid container spacing={2}>
@@ -18,74 +22,87 @@ function TeamList() {
       <hr />
       <Grid container spacing={3}>
         <Grid container item xs={6} direction="column">
-          <hr />
+          <br />
           <h4>BLUEBERRY</h4>
         </Grid>
         <Grid container item xs={1} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>7</h3>
         </Grid>
         <Grid container item xs={5} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>DONE</h3>
         </Grid>
       </Grid>
       <Grid container spacing={3}>
         <Grid container item xs={6} direction="column">
-          <hr />
+          <br />
           <h4>APPLE</h4>
         </Grid>
         <Grid container item xs={1} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>5</h3>
         </Grid>
         <Grid container item xs={5} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>NOT YET</h3>
         </Grid>
       </Grid>
       <Grid container spacing={3}>
         <Grid container item xs={6} direction="column">
-          <hr />
+          <br />
           <h4>CHERRY</h4>
         </Grid>
         <Grid container item xs={1} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>3</h3>
         </Grid>
         <Grid container item xs={5} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>DONE</h3>
         </Grid>
       </Grid>
       <Grid container spacing={3}>
         <Grid container item xs={6} direction="column">
-          <hr />
+          <br />
           <h4>LEMON</h4>
         </Grid>
         <Grid container item xs={1} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>3</h3>
         </Grid>
         <Grid container item xs={5} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>DONE</h3>
         </Grid>
       </Grid>
       <Grid container spacing={3}>
         <Grid container item xs={6} direction="column">
-          <hr />
+          <br />
           <h4>STRAWBERRY</h4>
         </Grid>
         <Grid container item xs={1} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>1</h3>
         </Grid>
         <Grid container item xs={5} direction="column">
-          <hr />
+          <br />
           <h3 style={{ textAlign: 'right' }}>NOT YET</h3>
         </Grid>
       </Grid>
+      <br />
+      <br />
+      <br />
+      <br />
+      {/* PUT BUTTONS IN A GRID */}
+      <Button sx={whiteButton} variant="outlined">
+        <h3>EXTENDS 30 SECONDS</h3>
+      </Button>
+      <br />
+      <br />
+      <Button sx={redButton} variant="outlined">
+        <h3>END ROUND</h3>
+      </Button>
     </>
   );
 }
