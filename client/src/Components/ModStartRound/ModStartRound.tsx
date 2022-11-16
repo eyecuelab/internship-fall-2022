877b1b43e669c2d3ad66f4b4bb1d02a3ac8292ee
Todd Pangilinan <pang.todd@gmail.com>
2022-11-16 00:12:35 -0800

    updated style to line up button on bottom correctly.

--- client/src/Components/ModStartRound/ModStartRound.tsx
+++ client/src/Components/ModStartRound/ModStartRound.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../index.css';
+import {Container, GridButton, GridHeader, GridStyle, } from './styles'
 import {Grid, Button} from '@mui/material';
 import {whiteButton, greenButton, redButton} from '../componentStyles';
 
@@ -9,59 +10,28 @@ function ModStartRound() {
   greenButton.width = '100%';
 
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="space-between"
-      alignItems="center"
-      sx={{
-        height: '100',
-        width: '100%',
-        margin: 'auto',
-        display: 'block',
-      }}
-    >
-      <Grid item xs={12} md={12} lg={12} sx={{height: '50%', width: '100%'}}>
-        <h3>round *insert number* topic</h3>
-        <hr />
-        <h1>passed-in topic</h1>
-        <br />
-      </Grid>
-
-      <Grid
-        item
-        xs={12}
-        // sx={{
-        //   height: '50%',
-        //   width: '100%',
-        //   // margin: 'auto',
-        //   // alignItems: 'flex-end',
-        // }}
-      >
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+    <>
+      <div>
+          <h3>round *insert number* topic</h3>
+          <hr />
+          <h1>passed-in topic</h1>
+          <br />
+     </div>
+      
+    <Container>
         <Button sx={greenButton}>
           <h3>start round</h3>
         </Button>
-        <br />
         <br />
         <Button sx={whiteButton}>
           <h3>back to selection</h3>
         </Button>
         <br />
-        <br />
         <Button sx={redButton}>
           <h3>end game</h3>
         </Button>
-      </Grid>
-    </Grid>
+  </Container>
+  </>
   );
 }
 
