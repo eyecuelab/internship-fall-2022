1f83ea380aa79d1dd738c8a69acdc716940c2e11
Todd Pangilinan <pang.todd@gmail.com>
2022-11-17 23:07:18 -0800

    updated logout button to route to mod. May also need to have it toggle auth off.

--- client/src/Components/ModOverlay/ModOverlay.tsx
+++ client/src/Components/ModOverlay/ModOverlay.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../index.css';
+import { Link, useLocation } from "react-router-dom";
 import { Grid, Button } from '@mui/material';
 
 function ModOverlay() {
@@ -20,22 +21,6 @@ function ModOverlay() {
         <h3>Team</h3>
         <h1>MODS</h1>
         <br />
-        {/* render when editing topics/phrases for a game */}
-        {/* <h3>Name</h3>
-          <h1>{'EyeCue Winter 2023'}</h1>
-          <br /> */}
-        {/* render while game is in progress */}
-        {/* <h3>Round</h3>
-          <h1>{'3'}</h1>
-          <br /> */}
-        {/* render during brainstorming phase each round */}
-        {/* <h3>Timer</h3>
-          <h1>{'47'}</h1>
-          <br /> */}
-        {/* render during guessing phase */}
-        {/* <h3>Teams left</h3>
-          <h1>{'4'}</h1>
-          <br /> */}
       </Grid>
       <Grid
         item
@@ -62,18 +47,20 @@ function ModOverlay() {
           <h3>some text</h3>
         </Button>
         <br />
-        <Button
-          sx={{
-            height: '5rem',
-            width: '100%',
-            color: '#363636',
-            border: '1px solid #363636',
-            borderRadius: '10px',
-            background: '#bbb',
-          }}
-        >
-          <h3>some text</h3>
-        </Button>
+        <Link to= "/mod"> 
+          <Button
+            sx={{
+              height: '5rem',
+              width: '100%',
+              color: '#363636',
+              border: '1px solid #363636',
+              borderRadius: '10px',
+              background: '#FC3911',
+            }} 
+          >
+            <h3>Logout</h3>
+          </Button>
+        </Link>
       </Grid>
     </Grid>
   );
