5ef1e0c5d50def7fd0b2636ed1d580f8bfaf26b9
Anthony DiFalco <aodifalco@gmail.com>
2022-11-18 10:01:51 -0800

    render data from db in gamelist component

--- client/src/Components/ModGameList/ModGameList.tsx
+++ client/src/Components/ModGameList/ModGameList.tsx
@@ -5,25 +5,43 @@ import { greenButton } from '../componentStyles';
 import { getData } from '../../ApiHelper';
 
 interface Props {
-	handleCreateNewGame: () => void;
+  handleCreateNewGame: () => void;
 }
 
-const getGames = async () => { 
-	const games = await getData("/games"); 
-	return games; 
-}
+const getGames = async () => {
+  const games = await getData('/games');
+  return games;
+};
 
 const gameList = await getGames();
+
+const renderGame = (game: any) => {
+	return (
+		<>
+			<Grid container item xs={5}>
+				<h4>{ game.name.toString() }</h4>
+			</Grid>
+			<Grid container item xs={3}>
+				<h3 style={{ width: '100%', textAlign: 'right'}}>{ game.publishedAt ? 'published' : 'pending' }</h3>
+			</Grid>
+			<Grid item xs={1}>
+				<IconButton aria-label="delete">
+					<Delete />
+				</IconButton>
+			</Grid>
+		</>
+	);
+}
 
 function ModGameList(props: Props) {
   greenButton.width = '100%';
 
-	console.log('LINE 26: ', gameList);
+  console.log('LINE 26: ', gameList);
 
   return (
     <>
-      <Grid container>
-        <Grid container item xs={7}>
+      <Grid container spacing={28}>
+        <Grid container item xs={6}>
           <h3>GAMES</h3>
         </Grid>
         <Grid container item xs={3}>
@@ -31,20 +49,16 @@ function ModGameList(props: Props) {
         </Grid>
       </Grid>
       <hr />
-      <Grid container>
-        <Grid container item xs={7}>
-          <h4>EVENT NAME</h4>
-        </Grid>
-        <Grid container item xs={4}>
-          <h3>PENDING</h3>
-        </Grid>
-				<Grid item xs={1} >
-					<IconButton aria-label="delete">
-						<Delete />
-					</IconButton>
-				</Grid >
+      <Grid container spacing={30}>
+				{/* @ts-ignore */}
+				{ <> { (gameList.map((game) => renderGame(game))) } </> }
       </Grid>
-      <Button onClick={props.handleCreateNewGame} sx={greenButton} style={{ position: 'relative', top: 480 }} variant="outlined">
+      <Button
+        onClick={props.handleCreateNewGame}
+        sx={greenButton}
+        style={{position: 'relative', top: 480}}
+        variant="outlined"
+      >
         <h3>CREATE A NEW GAME</h3>
       </Button>
     </>
