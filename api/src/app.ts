675647d11b1239b7c6a606de0fd93c65104c5f7f
Greg Khmara <gkhmara@gmail.com>
2022-11-22 16:45:28 -0800

    add ability to delete phrases

--- api/src/app.ts
+++ api/src/app.ts
@@ -15,7 +15,7 @@ app.use(cors({
     'http://localhost:5173',
 		'https://www.thunderclient.com'
   ], 
-  methods: ['GET', 'POST']}));
+  methods: ['GET', 'POST', 'DELETE']}));
 
 app.use(express.json());
 
