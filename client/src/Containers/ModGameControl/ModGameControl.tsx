4864043d3ea1827f39e38147b504a64cf41da463
Todd Pangilinan <pang.todd@gmail.com>
2022-11-26 15:53:35 -0800

    update ModGameControl so it handles state, instead of being done downstream.

--- client/src/Containers/ModGameControl/ModGameControl.tsx
+++ client/src/Containers/ModGameControl/ModGameControl.tsx
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../index.css';
+import { deleteData, getData } from '../../ApiHelper';
 import CardTemplate from '../../Components/CardTemplate/CardTemplate';
 import ModLogin from '../../Components/ModLogin/ModLogin';
 import ModGameList from '../../Components/ModGameList/ModGameList';
 import ModNewGame from '../../Components/ModNewGame/ModNewGame';
 import ModOverlay from '../../Components/ModOverlay/ModOverlay';
 
+const getGames = () => {
+  const games = getData('/games');
+  return games;
+};
+
+const gameList = await getGames();
+
 function ModGameControl() {
   const [login, setLogin] = useState(true);
   const [createNewGameView, setCreateNewGameView] = useState(false);
+  const [games, setGames] = useState([]);
+
+	useEffect(() => {
+		getGameList();
+	}, []);
+
+  const getGameList = async () => {
+		const gameList = await getData('/games');
+		setGames(gameList);
+	}
+
+  const deleteGame = (gameId: any)=> {
+    deleteData(`/games/${gameId}`).then(()=> getGameList());
+  }
 
 	document.documentElement.style.background = 'url(/images/moderator_background.png)';
-
+  
   const handleLogin = () => {
     setLogin(true);
   };
@@ -29,7 +51,7 @@ function ModGameControl() {
       return (
         <CardTemplate
           user="moderator"
-          content={<ModGameList handleCreateNewGame={handleCreateNewGame} />}
+          content={<ModGameList handleDeleteGame ={deleteGame} games= {games} handleCreateNewGame={handleCreateNewGame} />}
           overlay={<ModOverlay handleLogout={handleLogout} />}
         />
       );
