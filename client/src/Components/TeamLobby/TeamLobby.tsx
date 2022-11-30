5b25f0d4a32dbbb2466ea8ff9a851d9021104237
Anthony DiFalco <aodifalco@gmail.com>
2022-11-29 16:57:55 -0800

    add animations

--- client/src/Components/TeamLobby/TeamLobby.tsx
+++ client/src/Components/TeamLobby/TeamLobby.tsx
@@ -10,11 +10,11 @@ function TeamLobby() {
       sx={{ height: '100%', paddingTop: '15%', paddingBottom: '15%' }}
     >
       <Grid item>
-        <h3 style={{ width: '65%' }}>you are invited to be the team lead for</h3>
-        {/* <h3>team {'blueberry'},</h3><h3> get ready for</h3> */}
-        <h1 style={{ marginTop: '1rem' }}>{'team blueberry'}</h1>
+        <h3 className="fade-in-down" style={{ width: '65%' }}>you are invited to be the team lead for</h3>
+        {/* <h3 className="fade-in-down">team {'blueberry'},\n get ready for</h3> */}
+        <h1 className="fade-in-left" style={{ marginTop: '1rem' }}>{'team blueberry'}</h1>
       </Grid>
-      <h5>{'waiting for moderator to start the game...'}</h5>
+      <h5 className="fade-in-up">{'waiting for moderator to start the game...'}</h5>
     </Grid>
   );
 }
