47a0598077f861bbd9eb53dfd07e861d26099aef
Anthony DiFalco <aodifalco@gmail.com>
2022-11-30 20:40:50 -0800

    bugfix?: gamelist properly renders new games?

--- client/src/Containers/ModGameControl/ModGameControl.tsx
+++ client/src/Containers/ModGameControl/ModGameControl.tsx
@@ -23,6 +23,7 @@ function ModGameControl(props: Props) {
 	}, []);
 
 	const getGameList = () => {
+		setGames([]);
 		const user = JSON.parse(localStorage.getItem('user') as string);
 		if (user) {
 		getData(`/moderators/${user.email}`)
