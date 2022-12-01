513f5aeb4aae147503fb4bd6828f17976263e767
Winnie Wang <weijwang18@gmail.com>
2022-12-01 01:47:09 -0800

    WIP: Got TeamName and Score rendering, still need to work on Status.

--- client/src/Components/Moderators/TeamList/index.tsx
+++ client/src/Components/Moderators/TeamList/index.tsx
@@ -1,133 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../../../index.css';
-import { Grid, Button } from '@mui/material';
-import { whiteButton, redButton } from '../../componentStyles';
+import {Grid, Button} from '@mui/material';
+import {whiteButton, redButton} from '../../componentStyles';
+import {Container, ButtonContainer} from './styles';
+import {getData} from '../../../ApiHelper';
+import { Team } from '../../../Types/Types';
+import TeamItem from './TeamItem';
 
-function TeamList() {
+interface Props {
+//   handleSwitch?: () => void;
+//   gameId: number;
+}
+
+function TeamList(props: Props) {
+	const [teams, setTeams] = useState([]);
+
+	const getTeamList = async () => {
+		const TeamList = await getData(`/teams`);
+		setTeams(TeamList);
+	};
+
+	useEffect(() => {
+		getTeamList();
+	}, []);
+
   whiteButton.width = '100%';
   redButton.width = '100%';
+
+
   return (
     <>
-      <Grid
-        container
-        direction ="column"
-        justifyContent="space-between"
-        alignItems="center"
-        sx={{
-          height: '100%',
-          width: '100%',
-          margin: 'auto',
-          display: 'block',
-        }}
-      >
-        <Grid item xs={12} md={12} lg={12}>
-
+      <Container>
+        <Grid container>
+          <Grid container item xs={5} direction="column">
+            <h3>GAMES</h3>
+          </Grid>
+          <Grid container item xs={3} direction="column">
+            <h3 style={{textAlign: 'right'}}>SCORE</h3>
+          </Grid>
+          <Grid container item xs={4} direction="column">
+            <h3 style={{textAlign: 'right'}}>STATUS</h3>
+          </Grid>
         </Grid>
-      </Grid>
-
-
-
-
-
-
-
-
-
-      <Grid container spacing={2}>
-        <Grid container item xs={4} direction="column">
-          <h3>GAMES</h3>
-        </Grid>
-        <Grid container item xs={4} direction="column">
-          <h3 style={{ textAlign: 'right' }}>SCORE</h3>
-        </Grid>
-        <Grid container item xs={4} direction="column">
-          <h3 style={{ textAlign: 'right' }}>STATUS</h3>
-        </Grid>
-      </Grid>
-      <hr />
-      <Grid container spacing={3}>
-        <Grid container item xs={6} direction="column">
+        <div>
+          <hr />
+        </div>
+		<Grid container>
+				{teams?.map((team: Team) => { return <TeamItem key={team.id} team={team} /> })}
+			</Grid>
+        <ButtonContainer>
+          <Button sx={whiteButton}>
+            <h3>EXTENDS 30 SECONDS</h3>
+          </Button>
           <br />
-          <h4>BLUEBERRY</h4>
-        </Grid>
-        <Grid container item xs={1} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>7</h3>
-        </Grid>
-        <Grid container item xs={5} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>DONE</h3>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid container item xs={6} direction="column">
-          <br />
-          <h4>APPLE</h4>
-        </Grid>
-        <Grid container item xs={1} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>5</h3>
-        </Grid>
-        <Grid container item xs={5} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>NOT YET</h3>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid container item xs={6} direction="column">
-          <br />
-          <h4>CHERRY</h4>
-        </Grid>
-        <Grid container item xs={1} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>3</h3>
-        </Grid>
-        <Grid container item xs={5} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>DONE</h3>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid container item xs={6} direction="column">
-          <br />
-          <h4>LEMON</h4>
-        </Grid>
-        <Grid container item xs={1} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>3</h3>
-        </Grid>
-        <Grid container item xs={5} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>DONE</h3>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid container item xs={6} direction="column">
-          <br />
-          <h4>STRAWBERRY</h4>
-        </Grid>
-        <Grid container item xs={1} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>1</h3>
-        </Grid>
-        <Grid container item xs={5} direction="column">
-          <br />
-          <h3 style={{ textAlign: 'right' }}>NOT YET</h3>
-        </Grid>
-      </Grid>
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <Button sx={whiteButton} >
-        <h3>EXTENDS 30 SECONDS</h3>
-      </Button>
-      <br />
-      <br />
-      <Button sx={redButton} >
-        <h3>END ROUND</h3>
-      </Button>
+          <Button sx={redButton}>
+            <h3>END ROUND</h3>
+          </Button>
+        </ButtonContainer>
+      </Container>
     </>
   );
 }
