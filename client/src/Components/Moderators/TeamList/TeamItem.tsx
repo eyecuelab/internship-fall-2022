f6bda53a0f396daa139d690fb995a9adeefe625f
Winnie Wang <49203832+weijwang18@users.noreply.github.com>
2022-12-02 09:35:27 -0800

    Update TeamItem.tsx

--- client/src/Components/Moderators/TeamList/TeamItem.tsx
+++ client/src/Components/Moderators/TeamList/TeamItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Grid} from '@mui/material';
 import { Team } from '../../../Types/Types';
-import { Haicue } from '../../../Types/Types';
 
 interface Props {
 	team: Team,
@@ -9,7 +8,6 @@ interface Props {
 
 function TeamItem (props: Props) {
 	const { team } = props
-	const { haicue } = props
 
 	return(
 		<>
