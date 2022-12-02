32bab0f6285710f9e7ec1d046e67e4f1c745d6bd
Todd Pangilinan <pang.todd@gmail.com>
2022-12-01 22:06:57 -0800

    refactored conditional rendering to work off of passed prop '' q

--- client/src/Components/Moderators/Overlay/index.tsx
+++ client/src/Components/Moderators/Overlay/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../../index.css';
-import { Link, useLocation } from 'react-router-dom';
-import { Grid, Button } from '@mui/material';
-import { DogEarButton, greenButton, redButton } from '../../componentStyles';
-import { putData } from '../../../ApiHelper';
+import {Link, useLocation} from 'react-router-dom';
+import {Grid, Button} from '@mui/material';
+import {DogEarButton, greenButton, redButton, blackButton} from '../../componentStyles';
+import {putData} from '../../../ApiHelper';
 import GameInfo from './GameInfo';
 
 interface Props {
@@ -21,6 +21,9 @@ function ModOverlay(props: Props) {
 
   redButton.width = '100%';
   greenButton.width = '100%';
+  blackButton.width = '100%';
+
+  console.log(props.gameData.labelOne)
 
   return (
     <Grid
@@ -40,8 +43,12 @@ function ModOverlay(props: Props) {
         <h1>MODS</h1>
         <br />
       </Grid>
-      {props.gameData && <GameInfo h1Input={props.gameData.textOne} h3Input={props.gameData.labelOne}/>}
-      {props.gameData && <GameInfo h1Input={props.gameData.textTwo} h3Input={props.gameData.labelTwo}/>}
+      {props.gameData && (
+        <GameInfo h1Input={props.gameData.textOne} h3Input={props.gameData.labelOne} />
+      )}
+      {props.gameData && (
+        <GameInfo h1Input={props.gameData.textTwo} h3Input={props.gameData.labelTwo} />
+      )}
       <Grid
         item
         xs={12}
@@ -53,19 +60,31 @@ function ModOverlay(props: Props) {
         }}
       >
         <Link to="/">
-          {(location.pathname.includes('/topic/') || location.pathname.includes('/game/')) && (
-            <DogEarButton onClick={() => updateGameStatus(props.gameId)} style={greenButton}>
-              <h3>Publish</h3>
+          {props.gameData.labelOne == "game" && 
+            <>
+              <DogEarButton onClick={() => updateGameStatus(props.gameId)} style={greenButton}>
+                <h3>Publish</h3>
+              </DogEarButton>
+              <br />
+              <br />
+              <DogEarButton onClick={props.handleLogout} style={redButton}>
+                <h3>Logout</h3>
+              </DogEarButton>
+            </>
+          }
+          {location.pathname == '/' && 
+            <DogEarButton onClick={props.handleLogout} style={redButton}>
+              <h3>Logout</h3>
             </DogEarButton>
-          )}
+          }
         </Link>
         <br />
         <br />
-        <Link to="/">
-          <DogEarButton onClick={props.handleLogout} style={redButton}>
-            <h3>Logout</h3>
-          </DogEarButton>
-        </Link>
+        {props.gameData.labelOne== "round" && 
+            <DogEarButton onClick={props.handleLogout} style={blackButton}>
+              <h3>player url</h3>
+            </DogEarButton>
+          }
       </Grid>
     </Grid>
   );
