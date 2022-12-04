27501600fc401494dd14dfa624221ddfa4db4b61
Todd Pangilinan <pang.todd@gmail.com>
2022-12-03 18:26:25 -0800

    formatting.

--- client/src/Components/Moderators/Presenting/index.tsx
+++ client/src/Components/Moderators/Presenting/index.tsx
@@ -1,29 +1,28 @@
 import React, {useState} from 'react';
 import '../../../index.css';
-import { Container, ButtonContainer } from './styles';
-import { Button } from '@mui/material';
-import { whiteButton, greenButton, redButton, DogEarButton } from '../../componentStyles';
+import {Container, ButtonContainer} from './styles';
+import {Button} from '@mui/material';
+import {whiteButton, greenButton, redButton, DogEarButton} from '../../componentStyles';
 interface Props {
   handleSwitch?: () => void;
-  gameData?: any
-  haikuData?:any
-  ;
+  gameData?: any;
+  haikuData?: any;
 }
 
 function ModPresenting(props: Props) {
   whiteButton.width = '100%';
   redButton.width = '100%';
   greenButton.width = '100%';
-  console.log(props.haikuData)
-  const [lineNumber, setLineNumber] = useState(1)
+  console.log(props.haikuData);
+  const [lineNumber, setLineNumber] = useState(1);
 
-  const lineAdvancer =()=>{
-    if(lineNumber < 3){
-    setLineNumber(lineNumber + 1)
+  const lineAdvancer = () => {
+    if (lineNumber < 3) {
+      setLineNumber(lineNumber + 1);
     } else {
-    setLineNumber(1);
+      setLineNumber(1);
     }
-  }
+  };
 
   return (
     <>
@@ -34,7 +33,13 @@ function ModPresenting(props: Props) {
           <br />
           <br />
           <h3>line {lineNumber}</h3>
-          <h1>{lineNumber== 3? props.haikuData.line3 : lineNumber ==2? props.haikuData.line2 : props.haikuData.line1}</h1>
+          <h1>
+            {lineNumber == 3 ?
+              props.haikuData.line3 :
+              lineNumber == 2 ?
+              props.haikuData.line2 :
+              props.haikuData.line1}
+          </h1>
         </div>
         <ButtonContainer>
           <DogEarButton onClick={lineAdvancer} style={whiteButton}>
