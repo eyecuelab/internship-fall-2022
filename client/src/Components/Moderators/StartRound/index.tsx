1a0c8877e0d592fdad6c6de5ea281063d7ade893
Anthony DiFalco <aodifalco@gmail.com>
2022-12-04 09:36:43 -0800

    get props from url params

--- client/src/Components/Moderators/StartRound/index.tsx
+++ client/src/Components/Moderators/StartRound/index.tsx
@@ -10,46 +10,33 @@ import { postData, getData, putData } from '../../../ApiHelper';
 import socket from '../../../Hooks/WebsocketHook';
 
 interface Props {
-  gameId: number;
   topic: Topic;
   handleSwitch: Dispatch<SetStateAction<boolean>>;
 }
 
-interface IFormInput {
-  name: string;
-  gameId: number;
-  moderatorId: number;
-}
-
 function ModStartRound(props: Props) {
-	const { gameId, topic, handleSwitch} = props;
-  const { topicId } = useParams();
+	const { topic, handleSwitch} = props;
+  const { id } = useParams();
   const {setValue} = useForm<IFormInput>();
   const [topics, setTopics] = useState([]);
   const user = JSON.parse(localStorage.getItem('user') as string);
-
-  getData(`/moderators/${user.email}`).then(moderator => {
-    setValue('moderatorId', moderator.id);
-    setValue('gameId', gameId);
-  });
 
   useEffect(() => {
     getTopicList();
   }, []);
 
   const getTopicList = async () => {
-    getData(`/topics/game/${gameId}`).then((response) => {
+    getData(`/topics/game/${id}`).then((response) => {
 			setTopics(response);
 		});
   };
 
 	const selectTopic = () => {
-		postData('/addRound', { gameId: topic.gameId, topicId: topic.id}).then(() => {
-			getData(`/games/${topic.gameId}`).then((data) => {
+		postData('/addRound', { gameId: id, topicId: topic.id}).then(() => {
+			getData(`/games/${id}`).then((data) => {
 				localStorage.setItem('game', data);
-				const round = data.Rounds.slice(-1);
 				putData('/topics/', { topicId: topic.id, roundId: data.Rounds.slice(-1)[0].id });
-				postData('/startGame', { gameId: topic.gameId });
+				postData('/startGame', { gameId: id });
 			});
 		});
 	}
