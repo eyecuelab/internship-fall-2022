6bdfcd18dfacae86491f882cffadaddc6005c2fb
Todd Pangilinan <pang.todd@gmail.com>
2022-12-06 00:22:10 -0800

    WIP, set up initial sockets for buzzer." "

--- client/src/Components/Moderators/Presenting/index.tsx
+++ client/src/Components/Moderators/Presenting/index.tsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../../index.css';
 import {Container, ButtonContainer} from './styles';
+import socket from '../../../Hooks/WebsocketHook';
 import {Button} from '@mui/material';
 import {whiteButton, greenButton, redButton, DogEarButton} from '../../componentStyles';
+import { StartSharp } from '@mui/icons-material';
 interface Props {
   handleSwitch?: () => void;
   gameData?: any;
@@ -14,8 +16,27 @@ function ModPresenting(props: Props) {
   whiteButton.width = '100%';
   redButton.width = '100%';
   greenButton.width = '100%';
+
   const [lineNumber, setLineNumber] = useState(1);
-  
+
+  useEffect(()=>{
+    socket.on('connection', () => {
+			console.log('socket open');
+		});
+
+		socket.on('buzz', () => {
+			props.handleSwitch;
+		});
+
+    socket.emit('buzzer_refresh');
+    // this should ensure buzzers are refreshed anytime a guess is dismissed or a new round starts
+
+		return () => {
+			socket.off('connection');
+			socket.off('buzz');
+      // ask anthony about this return?
+		}
+  }, []);
 
   const lineAdvancer = () => {
     if (lineNumber < 3) {
@@ -24,8 +45,6 @@ function ModPresenting(props: Props) {
       setLineNumber(1);
     }
   };
-
-  console.log(props.topicData)
 
   return (
     <>
