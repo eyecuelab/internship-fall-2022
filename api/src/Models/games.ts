d03b80318a710c6ce43d13e7da1216838850428a
Anthony DiFalco <aodifalco@gmail.com>
2022-12-05 22:45:38 -0800

    WIP: fix api errors for rendering guessing phase data

--- api/src/Models/games.ts
+++ api/src/Models/games.ts
@@ -3,7 +3,7 @@ import GameCode from '../GenerateGameCode';
 
 const prisma = new PrismaClient();
 
-export const getGameById = async (id: number) => {
+export const getOneGame = async (id: number) => {
   return await prisma.games.findUnique({
 		where: {
 			id: Number(id)
